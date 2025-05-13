d3ef17892916547445bab292e0fc31ccb12be728
Vaidehi Dalmia <vaidehidalmia@Mac.attlocal.net>
2025-05-13 16:08:39 -0700

    added redis boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const { createClient } = require('redis');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (_, res) => res.send('Hello Url Shortner'));
+const client = createClient({ url: process.env.REDIS_URL });
+
+client.connect()
+    .then(() => console.log('COnnected to Redis'))
+    .catch(err => console.error('Redis connection error: ', err));
+
+app.get('/', async (req, res) => {
+    await client.set('test_key', 'Redis is working!');
+    const message = await client.get('test_key');
+    res.send(message);
+});
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
